a93470dce21867ae44290c926fde609f2b1a6448
Piyush Bharti <piyushbharti338@gmail.com>
2026-07-07 10:51:05 +0530

    Use MongoDB Atlas URI

--- app/db/connectDb.js
+++ app/db/connectDb.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    
-        try {
-            const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, 
-              
-            );
-            console.log(`MongoDB Connected: ${conn.connection.host}`);
-            return conn;
-            
-        } catch (error) {
-            console.error(error.message);
-            process.exit(1);
-        }
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
-  export default connectDb;
+export default connectDb;
